a25f28f3d5be0792d2f0d2063db83ec24fdd9469
starbro85 <starbro85@gmail.com>
2020-05-20 04:32:50 +0700

    Add bullet theme for list component. Other minor fixes or it.

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,1 +1,23 @@
 import './list.css';
+
+class List {
+    constructor(node) {
+        this.root = node;
+        this.theme = this.root.dataset.theme;
+
+        this.init();
+    }
+
+    init() {
+        this.root.classList.add('list_theme_' + this.theme);
+    }
+};
+
+function render() {
+    const components = document.body.querySelectorAll('.js-list');
+    if (components.length > 0) {
+        Array.from(components).map((node) => new List(node));
+    };
+};
+
+render();
