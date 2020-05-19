020449f7b5ff3ae0128a5ce2f09bb4272c222321
starbro85 <starbro85@gmail.com>
2020-05-20 04:32:02 +0700

    Add rich theme for checkbox component. Other minor fixes for it.

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -1,1 +1,23 @@
 import './checkbox.css';
+
+class Checkbox {
+    constructor(node) {
+        this.root = node;
+        this.theme = this.root.dataset.theme;
+
+        this.init();
+    }
+
+    init() {
+        this.root.classList.add('checkbox_theme_' + this.theme);
+    }
+};
+
+function render() {
+    const components = document.body.querySelectorAll('.js-checkbox');
+    if (components.length > 0) {
+        Array.from(components).map((node) => new Checkbox(node));
+    };
+};
+
+render();
