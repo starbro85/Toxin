c25b9be182cc5df3525254b40f36223f36b5bf66
starbro85 <starbro85@gmail.com>
2020-07-01 22:26:25 +0700

    Quantity-dropdown component changes (see below) - transfer counter component into quantity-dropdown - make the counter a child component of quantity-dropdown - accessibility-edits for counter - transfer dropdown logic into quantity-dropdown - change counter-data accumulation logic

---
+++ src/components/quantity-dropdown/counter/counter.js
@@ -0,0 +1,86 @@
+import './counter.css';
+
+class Counter {
+    constructor(node) {
+        this.root = node;
+        this.decrement = this.root.querySelector('.js-counter__decrement');
+        this.increment = this.root.querySelector('.js-counter__increment');
+        this.input = this.root.querySelector('.js-counter__input');
+        this.value = Number(this.input.value);
+        this.minValue = Number(this.input.getAttribute('aria-valuemin'));
+        this.maxValue = Number(this.input.getAttribute('aria-valuemax'));
+
+        this.init();
+    }
+
+    normalizeRange() {
+        this.increment.disabled = this.value >= this.maxValue;
+        this.decrement.disabled = this.value <= this.minValue;
+    }
+
+    sendCounterData = () => {
+        const name = this.input.name;
+        const value = Number(this.input.value);
+        const plural = JSON.parse(this.input.dataset.plural);
+        const isBound = Boolean(this.input.dataset.isBound);
+        const boundName = isBound ? this.input.dataset.boundName : '';
+        const boundPlural = isBound ? JSON.parse(this.input.dataset.boundPlural) : '';
+
+        this.root.dispatchEvent(new CustomEvent('counter-data-sent', {
+            detail: {
+                name: name,
+                value: value,
+                plural: plural,
+                isBound: isBound,
+                boundName: boundName,
+                boundPlural: boundPlural   
+            }
+        }));
+    }
+
+    handleCounterChange = (event) => {
+        if (event.target === this.increment) {
+            this.value = this.value + 1;
+            this.input.focus();
+        }
+
+        if (event.target === this.decrement) {
+            this.value = this.value - 1;
+            this.input.focus();
+        }
+
+        if (event.key === 'ArrowUp') {
+            this.increment.click();
+        }
+
+        if (event.key === 'ArrowDown') {
+            this.decrement.click();
+        }
+
+        this.input.value = this.value;
+        this.input.setAttribute('aria-valuenow', this.value);
+
+        this.sendCounterData();
+        this.normalizeRange();
+    }
+
+    handleCounterClear = () => {
+        this.value = 0;
+        this.input.value = this.value;
+
+        this.sendCounterData();
+        this.normalizeRange();
+    }
+
+    init() {
+        this.normalizeRange();
+
+        window.addEventListener('load', this.sendCounterData);
+        this.root.addEventListener('counter-value-clear', this.handleCounterClear)
+        this.increment.addEventListener('click', this.handleCounterChange);   
+        this.decrement.addEventListener('click', this.handleCounterChange);
+        this.input.addEventListener('keyup', this.handleCounterChange);
+    }
+};
+
+export {Counter};
--- src/components/quantity-dropdown/quantity-dropdown.js
+++ src/components/quantity-dropdown/quantity-dropdown.js
@@ -1,23 +1,100 @@
 import './quantity-dropdown.css';
+import './counter/counter.css';
 
-import './../counter-bar/counter-bar.js';
-import './../dropdown/dropdown.js';
+import './../text-field/text-field.js';
+
+import { Counter } from './counter/counter.js';
 
 const normalizeStr = require('./../../globals/helpers/normalizeStr.js');
 const pluralize = require('./../../globals/helpers/pluralize.js');
+const render = require('./../../globals/helpers/render.js');
 
-class Quantity {
+class Dropdown {
     constructor(node) {
         this.root = node;
-        this.input = this.root.querySelector('.js-quantity-dropdown__input');
-        this.countersBar = this.root.querySelector('.js-quantity-dropdown__counter-bar');
-        this.counterData = {};
+        this.button = this.root.querySelector('.js-quantity-dropdown__button');
+        this.container = this.root.querySelector('.js-quantity-dropdown__container');
+        this.focusableElements = this.container.querySelectorAll('button:not(:disabled):not([tabindex="-1"]), input:not(:disabled):not([tabindex="-1"])');
+        this.firstFocusableElement = this.focusableElements[0];
+        this.lastFocusableElement = this.focusableElements[this.focusableElements.length - 1];
+
+        this.expanded = JSON.parse(this.button.getAttribute('aria-expanded'));
 
         this.init();
     }
 
+    handleCollapse = (event) => { 
+        if (event.key === 'Escape') {
+            this.toggleExpand();
+            this.button.focus();
+        }
+
+        if (!this.root.contains(event.target)) {
+            this.toggleExpand();
+        }
+    }
+
+    toggleExpand = () => {
+        this.expanded = !(this.expanded);
+        this.button.setAttribute('aria-expanded', this.expanded);
+        this.root.classList.toggle('quantity-dropdown_expanded');
+        this.expanded ? document.addEventListener('click', this.handleCollapse) : document.removeEventListener('click', this.handleCollapse);
+        this.focusableElements.forEach(element => this.expanded ? element.addEventListener('keyup', this.handleCollapse) : element.removeEventListener('keyup', this.handleCollapse));
+        this.lastFocusableElement.addEventListener('blur', () => this.expanded ? this.firstFocusableElement.focus() : this.firstFocusableElement.focus());
+    };
+
+    init() {
+        this.button.addEventListener('click', this.toggleExpand);
+    }
+};
+
+class Quantity {
+    constructor(node) {
+        this.root = node;
+        this.textField = this.root.querySelector('.js-quantity-dropdown__text-field');
+        this.counters = this.root.querySelectorAll('.js-counter');
+
+        this.autoApply = Boolean(this.root.dataset.autoApply);
+        this.countersData = {};
+
+        this.init();
+    }
+      
+    getCountersData = () => {
+        const name = event.detail.name;
+        const value = event.detail.value;
+        const plural = event.detail.plural;
+        const isBound = event.detail.isBound;
+        const boundplural = event.detail.boundPlural;
+        const boundName = event.detail.boundName;
+
+        if (isBound) {
+            if (!this.countersData[boundName])
+                this.countersData[boundName] = {
+                    name: boundName,
+                    isBound: isBound,
+                    plural: boundplural,
+                    originData: {}
+                }
+            this.countersData[boundName].originData[name] = { name: name };
+            this.countersData[boundName].originData[name].plural = plural;
+            this.countersData[boundName].originData[name].value = value;
+            this.countersData[boundName].value = Object.values(this.countersData[boundName].originData)
+                                                    .reduce((sumValue, data) => sumValue + data.value, 0);
+        }
+
+        else {
+            this.countersData[name] = {
+                name: name,
+                plural: plural,
+                value: value,
+                isBound: isBound
+            }
+        }
+    }
+
     getInputSizeInChar() {
-        const style = getComputedStyle(this.input);
+        const style = getComputedStyle(this.textField);
         const inputWidth = parseInt(style.width);
         const inputSizeInChar = Math.floor(inputWidth * 0.125);
 
@@ -25,71 +102,92 @@ class Quantity {
     }
     
     getInputValue() {
-        const counters = Object.values(this.counterData);
-        return counters.reduce((inputValue, counter) => (counter.value !== 0) ? `${inputValue}${pluralize(counter.plural, counter.value)}, ` : `${inputValue}`, '')
-                            .slice(0, -2);
-    }
+        const counters = Object.values(this.countersData);
+        const inputValue = counters
+            .reduce((inputValue, counter) => (counter.value !== 0) ? `${inputValue}${pluralize(counter.plural, counter.value)}, ` : `${inputValue}`, '')
+            .slice(0, -2);
 
-    getInputTitle() {
-        const counters = Object.values(this.counterData);
-
-        return counters.reduce((inputTitle, counter) => {
-            if (counter.isBound) {
-                for (let name in counter.values) {
-                    inputTitle = (counter.values[name] !== 0) ? `${inputTitle}${pluralize(counter.plurals[name], counter.values[name])}, ` : `${inputTitle}`;
-                }       
-                return inputTitle; 
-            }
-            else {
-                return (counter.value !== 0) ? `${inputTitle}${pluralize(counter.plural, counter.value)}, ` : `${inputTitle}`;
-            }
-        }, '').slice(0, -2);
+        return inputValue;
     }
 
     getSubmitValue() {
-        const counters = Object.values(this.counterData);
-        const submitValue = counters.reduce((submitValue, counter) => counter.value ? `${submitValue}"${counter.name}": "${counter.value}", ` : `${submitValue}`, '').slice(0, -2);
+        const counters = Object.values(this.countersData);
+        const submitValue = counters
+            .reduce((submitValue, counter) => counter.value ? `${submitValue}"${counter.name}": "${counter.value}", ` : `${submitValue}`, '')
+            .slice(0, -2);
 
         return submitValue ? `{${submitValue}}` : '';
     }
 
-    addUpdateInputValueEvent() {
-        const inputValueNormalized = normalizeStr({
+    sendInputValue = () => {
+        const inputValue = normalizeStr({
                                         str: this.getInputValue(),
                                         size: this.getInputSizeInChar()
                                     });
-        const inputTitle = this.getInputTitle();
+        const title = this.getInputValue();
         const hiddenInputValue = this.getSubmitValue();
 
-        this.input.dispatchEvent(new CustomEvent('update-input-value', {
+        this.textField.dispatchEvent(new CustomEvent('text-field-value-sent', {
             detail: {
-                value: inputValueNormalized,
-                title: inputTitle,
+                value: inputValue,
+                title: title,
                 submitValue: hiddenInputValue
             }
         }));
+    };
+
+    setClearButtonDisabledState() {
+        const counters = Object.values(this.countersData);
+        const isDisabled = counters.reduce((isDisabled, data) => data.value > 0 ? isDisabled = false : isDisabled , true);
+
+        this.clearButton.disabled = isDisabled;
     }
 
-    handleDataSentEvent = event => {
-        this.counterData = event.detail.counterData;
+    handleApplyValue = (event) => {
+        if (event.target === this.clearButton) {
+            this.counters.forEach(counter => counter.dispatchEvent(new CustomEvent('counter-value-clear')));
 
-        this.addUpdateInputValueEvent();
+            this.sendInputValue(); 
+        }
+
+        if (event.target === this.applyButton) {
+            this.sendInputValue();    
+        }
+
+        this.setClearButtonDisabledState();
     }
 
-    setDataSentEventListener() {
-        this.countersBar.addEventListener('data-sent', this.handleDataSentEvent);
+    setManualApplyMode() {
+        this.clearButton = this.root.querySelector('.js-quantity-dropdown__clear-button');
+        this.applyButton = this.root.querySelector('.js-quantity-dropdown__apply-button');
+
+        this.clearButton.addEventListener('click', this.handleApplyValue);
+        this.applyButton.addEventListener('click', this.handleApplyValue);
+
+        this.setClearButtonDisabledState();
     }
+
+    setAutoApplyMode() {
+        this.counters.forEach(counter => counter.addEventListener('counter-data-sent', this.sendInputValue));
+    }
+
+    setCountersDataSentEventListener() {
+        this.counters.forEach(counter => counter.addEventListener('counter-data-sent', this.getCountersData));
+    } 
 
     init() {
-        this.setDataSentEventListener();
+        this.setCountersDataSentEventListener();
+
+        if (this.autoApply) {
+            this.setAutoApplyMode();
+        }
+
+        else {
+            this.setManualApplyMode();
+        }
     }
 };
 
-function render() {
-    const components = document.body.querySelectorAll('.js-quantity-dropdown');
-    if (components.length > 0) {
-        Array.from(components).map((node) => new Quantity(node));
-    };
-};
-
-render();
+render('.js-counter', Counter);
+render('.js-quantity-dropdown', Dropdown);
+render('.js-quantity-dropdown', Quantity);
