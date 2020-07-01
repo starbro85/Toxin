a1ffed95cfe1ab88c85397cda31040034dabcd30
starbro85 <starbro85@gmail.com>
2020-07-01 23:29:53 +0700

    Changes for datepicker and date-dropdown - temporary remove date-dropdown's logic - add accessibility features for datepicker

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -1,102 +1,21 @@
 import './date-dropdown.css';
 
-import '../dropdown/dropdown.js';
 import '../datepicker/datepicker.js';
-
+ 
 import moment from 'moment';
+
+const render = require('./../../globals/helpers/render.js');
 
 class DateDropdown {
     constructor(node) {
         this.root = node;
-        this.isTwin = this.root.dataset.twin;
-        this.datepicker = this.root.querySelector('.js-date-dropdown__datepicker');
-
-        if (this.isTwin) {
-            this.input = this.root.querySelectorAll('.js-date-dropdown__input');
-        }
-        else {
-            this.input = this.root.querySelector('.js-date-dropdown__input');
-        }
-
-
-        this.dateDropdownData = {};
-        this.datepickerData = {};
 
         this.init();
     }
 
-    getInputValue() {
-        if (this.isTwin) {
-            return this.datepickerData.date ? this.datepickerData.date.map(item => moment(item).format('DD.MM.YYYY')) : ['', ''];
-        }
-
-        else {
-            return this.datepickerData.date ? this.datepickerData.date.reduce((acc, item) => `${acc}${moment(item).format('DD.MM.YYYY')} - `, '').slice(0, -2) : '';
-        }
-    }
-
-    getDateDropdwonData() {
-        this.dateDropdownData = {
-            startDate: this.datepickerData.date ? this.datepickerData.date[0] : '',
-            endDate: this.datepickerData.date ? this.datepickerData.date[1] : '',
-            dayRange: this.datepickerData.date ? (this.datepickerData.date[1] - this.datepickerData.date[0]) / 1000 / 60 / 60 / 24 : 0
-        }
-    }
-
-    addUpdateInputValueEvent() {
-        const inputValue = this.getInputValue();
-
-        if (this.isTwin) {
-            this.input.forEach((item, index) => 
-                item.dispatchEvent(new CustomEvent('update-input-value', {
-                    detail: {
-                        value: inputValue[index],
-                        title: inputValue[index],
-                        submitValue: this.datepickerData.date ? this.datepickerData.date[index] : ''
-                    }
-                }))
-            )
-        }
-        else {
-            this.input.dispatchEvent(new CustomEvent('update-input-value', {
-                detail: {
-                    inputValue: inputValue,
-                    inputTitle: inputValue,
-                    hiddenInputValue: this.datepickerData.date
-                }
-            }));
-        }
-    }
-
-    addDataSentEvent() {
-        this.getDateDropdwonData();
-
-        this.root.dispatchEvent(new CustomEvent('data-sent', {
-            detail: {
-                dateDropdownData: this.dateDropdownData
-            }
-        }));
-    }
-
-    setDataSentEventListener() {
-        this.datepicker.addEventListener('data-sent', event => { 
-            this.datepickerData = event.detail.datepickerData;
-
-            this.addUpdateInputValueEvent();
-            this.addDataSentEvent();
-        });
-    }
-
     init() {
-        this.setDataSentEventListener();
+        
     }
 };
 
-function render() {
-    const components = document.body.querySelectorAll('.js-date-dropdown');
-    if (components.length > 0) {
-        Array.from(components).map((node) => new DateDropdown(node));
-    };
-};
-
-render();
+render('.js-date-dropdown', DateDropdown);
--- src/components/datepicker/datepicker.js
+++ src/components/datepicker/datepicker.js
@@ -1,95 +1,84 @@
 import './datepicker.css';
 
 import Litepicker from 'litepicker/dist/js/main.nocss';
+import 'litepicker-module-navkeyboard';
+import moment from 'moment';
+
+const render = require('./../../globals/helpers/render.js');
+const capitalize = require('./../../globals/helpers/capitalize.js');
 
 class Datepicker {
     constructor(node) {
         this.root = node;
-        this.main = this.root.querySelector('.js-datepicker__main');
-        this.clearButton = this.root.querySelector('.js-datepicker__clear-button');
-        this.applyButton = this.root.querySelector('.js-datepicker__apply-button');
-
-        this.defaultValue = this.root.dataset.defaultValue? JSON.parse(this.root.dataset.defaultValue) : '';
-
-        this.datepickerData = {};
+        this.initValue = this.root.dataset.initValue ? JSON.parse(this.root.dataset.initValue) : '';
+        this.lang = this.root.dataset.lang;
 
         this.init();
     }
 
-    handleUpdateDatepickerData = (date1, date2) => {
-        this.datepickerData = {
-            date: [date1, date2]
-        };
-    };
+    sendDatepickerData = (date1, date2) => {
+        const date = [date1, date2];
 
-    addDataSentEvent() {
-        this.root.dispatchEvent(new CustomEvent('data-sent', {
-            detail: {
-                datepickerData: this.datepickerData
-            }
+        this.root.dispatchEvent(new CustomEvent('datepicker-data-sent', {
+            date: date
         }));
     }
 
-    setClearButtonDisabledState() {
-        const isDisabled = this.datepickerData.date ? false : true;
+    setAriaAttributesToControls = (element, lang) => {
+        const prevButton = element.querySelector('.button-previous-month');
+        const nextButton = element.querySelector('.button-next-month');
 
-        this.clearButton.disabled = isDisabled;
+        const labels = {};
+
+        if (lang === 'ru') {
+            labels.prevButton = 'Перейти к предыдущему месяцу';
+            labels.nextButton = 'Перейти к следующиму месяцу';
+        }
+
+        if (lang === 'en') {
+            labels.prevButton = 'Go to prev month'
+            labels.nextButton = 'Go to next month';
+        }
+
+        prevButton.setAttribute('title', labels.prevButton);
+        prevButton.setAttribute('aria-label', labels.prevButton);
+        nextButton.setAttribute('title', labels.nextButton);
+        nextButton.setAttribute('aria-label', labels.nextButton);
     }
 
-    handleManualApplyEvent = event => {
-        if (Object.is(event.target, this.applyButton)) {
-            this.addDataSentEvent();
-        }
+    setAriaAttributesToCells = (element, lang) => {
+        const timestump = Number(element.dataset.time);
+        const date = capitalize(moment(timestump).locale(lang).format('dddd, LL'));
 
-        if (Object.is(event.target, this.clearButton)) {
-            this.datepickerData = {};
-            this.addDataSentEvent();    
-        }
-
-        this.setClearButtonDisabledState();
-    };
+        element.setAttribute('aria-label', date);
+        element.setAttribute('title', date);
+    }
 
     init() {
-        const lp = new Litepicker({
+        new Litepicker({
             element: this.root,
-            parentEl: this.main,
-            lang: 'ru',
+            parentEl: this.root,
+            lang: this.lang,
             inlineMode: true,
             numberOfMonths: 1,
             singleMode: false,
             format: 'DD.MM.YYYY',
-            startDate: this.defaultValue ? this.defaultValue[0] : null,     
-            endDate: this.defaultValue ? this.defaultValue[1]: null,
+            startDate: this.initValue ? this.initValue[0] : null,     
+            endDate: this.initValue ? this.initValue[1]: null,
             showTooltip: false,
             mobileFriendly: false,
-            onSelect: this.handleUpdateDatepickerData,
+            scrollToDate: false,
+            moveByOneMonth: true,
+            onRender: (element) => this.setAriaAttributesToControls(element, this.lang),
+            onRenderDay: (element) => this.setAriaAttributesToCells(element, this.lang),
+            onSelect: this.sendDatepickerData,
+            moduleNavKeyboard: true,
             buttonText: {
                 previousMonth: 'arrow_back',
                 nextMonth: 'arrow_forward'
             }
         })
-
-        if (this.defaultValue) {
-            lp.setDateRange(this.defaultValue[0], this.defaultValue[1]);
-
-            window.addEventListener('load', event => this.addDataSentEvent());
-        }
- 
-        this.setClearButtonDisabledState();
-
-        this.clearButton.addEventListener('click', event => {
-            lp.clearSelection();
-            this.handleManualApplyEvent(event);
-        });
-        this.applyButton.addEventListener('click', this.handleManualApplyEvent);
     }
 };
 
-function render() {
-    const components = document.body.querySelectorAll('.js-datepicker');
-    if (components.length > 0) {
-        Array.from(components).map((node) => new Datepicker(node));
-    };
-};
-
-render();
+render('.js-datepicker', Datepicker);
