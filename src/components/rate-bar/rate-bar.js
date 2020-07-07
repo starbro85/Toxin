33d7a635a01294e512c38e17dc1158ac1e48a33b
starbro85 <starbro85@gmail.com>
2020-07-07 13:32:28 +0700

    Remove auto-submit feature from rate-bar.

--- src/components/rate-bar/rate-bar.js
+++ src/components/rate-bar/rate-bar.js
@@ -1,36 +1,3 @@
 import './rate-bar.css';
 
-import './rate-button/rate-button.js';
-
-const render = require('./../../globals/helpers/render.js');
-
-class RateBar {
-    constructor(node) {
-        this.root = node;
-        this.rateButtons = this.root.querySelectorAll('.js-rate-button');
-
-        this.init();
-    }
-
-    setFormSubmitEvent() {
-        this.rateButtons.forEach(button => button.addEventListener('keyup', event => {
-            if ((event.key === 'Enter') || (event.key === 'Backspace')) {
-                this.root.submit();
-            }
-        }));
-
-        this.rateButtons.forEach(button => button.addEventListener('mouseup', event => {
-            if (event.which === 1) {
-                button.checked = true;
-
-                this.root.submit();
-            }
-        }));
-    }
-    
-    init() {
-        this.setFormSubmitEvent();
-    }
-};
-
-render('.js-rate-bar', RateBar);
+import './rate-button/rate-button.css';
