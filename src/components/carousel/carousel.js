1baad5e7c8513be5a9ae559b85559991cfe30a39
starbro85 <starbro85@gmail.com>
2020-07-20 00:43:33 +0700

    Some little changes. Remove unused webpack-plugins.

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,9 +1,9 @@
 import './carousel.css';
 
-import { Swiper, Navigation, Autoplay } from 'swiper';
+import { Swiper, Navigation, Autoplay, Lazy } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
 
-Swiper.use([Navigation, Autoplay]);
+Swiper.use([Navigation, Autoplay, Lazy]);
 
 const render = require('./../../globals/helpers/render.js');
 
@@ -12,47 +12,45 @@ class Carousel {
         this.root = node;
         this.controls = this.root.querySelector('.js-carousel__controls');
         
-        this.autoplay = Number(this.root.dataset.autoplayDelay);
+        this.autoplayDelay = Number(this.root.dataset.autoplayDelay);
 
         this.init();
     }
 
     init() {
-        window.addEventListener('load', (event) => {
-            const swiper = new Swiper(this.root, {
-                navigation: {
-                    nextEl: '.js-carousel__control-button_next',
-                    prevEl: '.js-carousel__control-button_prev',
-                },
-                autoplay: {
-                    delay: this.autoplay ? this.autoplay : false,
-                },
-                containerModifierClass: 'carousel_',
-                wrapperClass: 'carousel__slides',
-                slideClass: 'carousel__slide',
-                slideActiveClass: 'carousel__slide_active',
-                slideDuplicateActiveClass: 'carousel__duplicate-slide_active',
-                slideVisibleClass: 'carousel__slide_visible',
-                slideDuplicateClass: 'carousel__duplicate-slide',
-                slideNextClass: 'carousel__slide_next',
-                slideDuplicateNextClass: 'carousel__duplicate-slide_next',
-                slidePrevClass: 'carousel__slide_prev',
-                slideDuplicatePrevClass: 'carousel__duplicate-slide_prev',
-                slideBlankClass: 'carousel__slide_blank'
+        const swiper = new Swiper(this.root, {
+            navigation: {
+                nextEl: '.js-carousel__control-button_next',
+                prevEl: '.js-carousel__control-button_prev',
+            },
+            autoplay: this.autoplayDelay ? {
+                delay: this.autoplayDelay
+            } : false,
+            containerModifierClass: 'carousel_',
+            wrapperClass: 'carousel__slides',
+            slideClass: 'carousel__slide',
+            slideActiveClass: 'carousel__slide_active',
+            slideDuplicateActiveClass: 'carousel__duplicate-slide_active',
+            slideVisibleClass: 'carousel__slide_visible',
+            slideDuplicateClass: 'carousel__duplicate-slide',
+            slideNextClass: 'carousel__slide_next',
+            slideDuplicateNextClass: 'carousel__duplicate-slide_next',
+            slidePrevClass: 'carousel__slide_prev',
+            slideDuplicatePrevClass: 'carousel__duplicate-slide_prev',
+            slideBlankClass: 'carousel__slide_blank'
+        })
+
+        if (this.controls) {
+            this.root.addEventListener('keyup', event => {
+                if (event.key === 'ArrowRight') {
+                    swiper.slideNext();
+                }
+
+                if (event.key === 'ArrowLeft') {
+                    swiper.slidePrev();
+                }
             })
-
-            if (this.controls) {
-                this.controls.addEventListener('keyup', event => {
-                    if (event.key === 'ArrowRight') {
-                        swiper.slideNext();
-                    }
-
-                    if (event.key === 'ArrowLeft') {
-                        swiper.slidePrev();
-                    }
-                })
-            }    
-        }); 
+        }    
     }
 };
 
--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,1 +1,7 @@
 import './filter.css';
+
+import '../slider/slider.js';
+import '../date-dropdown/date-dropdown.js';
+import '../quantity-dropdown/quantity-dropdown.js';
+import '../checkbox-list/checkbox-list.js';
+import '../expandable-checkbox-list/expandable-checkbox-list.js';
